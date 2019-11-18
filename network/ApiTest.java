29ee9c6b656eaeb54ee2f1c30fb20bf254811689
massu <switch153r81953@icloud.com>
2019-11-18 12:00:35 +0900

    gradleと通信機構を設定

--- network/ApiTest.java
+++ network/ApiTest.java
@@ -4,7 +4,7 @@ import java.util.*;
 import com.google.gson.*;
 import com.squareup.okhttp.*;
 
-class ApiTest {
+public class ApiTest {
 
     public static void main(String[] args) {
         try {
